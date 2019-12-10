6f3ee8c6ae1bbb00fd7dc0b5f550d76a480bd0fc
tuvsheezz <tuvshee0089@gmail.com>
2019-12-10 17:05:36 +0800

    final 2

--- post/serializers.py
+++ post/serializers.py
@@ -47,7 +47,7 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
         files_data = self.context.get('view').request.FILES
         for file_data in files_data.values():
             PostFile.objects.create(post=post,type=str(file_data).split('.')[-1],file=file_data)
-
+            print(file_data)
         return post
 
     def update(self, instance, validated_data):
